989cd9df6f2946af57a23746ce7b85ab2a091171
danntracey329 <danntracey@icloud.com>
2020-10-10 22:38:55 +0100

    tutorial update part 2

--- FlaskBlog.py
+++ FlaskBlog.py
@@ -1,8 +1,27 @@
-from flask import Flask, escape, request
-
+from flask import Flask, escape, request, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello Worlds"
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'Fist post content',
+        'date_posted': 'April 20, 2018'
 
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
