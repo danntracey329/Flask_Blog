83ba18b7b1cfd951b4e5d91aa8205599cd982c0d
danntracey329 <danntracey@icloud.com>
2020-10-17 08:54:28 +0100

    tutorial update part 11
    Blueprint restructor

---
+++ FlaskBlog/config.py
@@ -0,0 +1,10 @@
+
+
+class Config:
+    SECRET_KEY = '123MANFORDS329'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'Smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'danntracey@googlemail.com'
+    MAIL_PASSWORD = 'trfolqgkuotjbbsk'
